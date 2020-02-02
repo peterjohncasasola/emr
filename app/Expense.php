f4df420ef75b651708471de15d456ae5cf02d964
GenErik <ebsupnet@gmail.com>
2020-02-02 15:45:02 +0800

    set up assets files v5

--- app/Expense.php
+++ app/Expense.php
@@ -9,5 +9,19 @@ class Expense extends Model
 {
     protected $primaryKey = 'id';
     protected $table = "expenses";
-
+    protected $cast = [
+        'salarieswages'=>'double',
+        'employeebenefits'=>'double',
+        'allowances'=>'double',
+        'totalps'=>'double',
+        'totalamountmedicine'=>'double',
+        'totalamountmedicalsupplies'=>'double',
+        'totalamountutilities'=>'double',
+        'totalamountnonmedicalservice'=>'double',
+        'totalmooe'=>'double',
+        'amountinfrastructure'=>'double',
+        'amountequipment'=>'double',
+        'totalco'=>'double',
+        'reportingyear'=>'int'
+    ];
 }
