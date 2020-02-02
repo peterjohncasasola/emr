3fa637c9824b690545689e32146a33e7d273112c
GenErik <ebsupnet@gmail.com>
2020-02-02 15:50:52 +0800

    set up assets v6

--- resources/views/layout/scripts.blade.php
+++ resources/views/layout/scripts.blade.php
@@ -73,4 +73,84 @@
 
 
 
+
+
+<!-- FOR REMOTE ONLY -->
+
+
+<!-- scripit init-->
+
+<!-- Additional plugins -->
+
+<!-- Angularjs -->
+<script type="text/javascript" src="{{asset('public/node_modules/angular/angular.min.js')}}"></script>
+
+<!-- Router -->
+<script type="text/javascript" src="{{asset('public/node_modules/angular-ui-router/release/angular-ui-router.min.js')}}"></script>
+
+<!-- Sanitize -->
+<script type="text/javascript" src="{{asset('public/node_modules/angular-sanitize/angular-sanitize.min.js')}}"></script>
+
+<!-- Auto Complete -->
+<!-- <script type="text/javascript" src="{{asset('node_modules/angular-auto-complete/angular-auto-complete.js')}}"></script> -->
+
+<!-- Angular-dynamic-number -->
+<script type="text/javascript" src="{{asset('public/node_modules/angular-dynamic-number/release/dynamic-number.min.js')}}"></script>
+
+
+<!-- bootstrap -->
+<script type="text/javascript" src="{{asset('public/node_modules/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+
+<!-- angular-ui -->
+<script type="text/javascript" src="{{asset('public/node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js')}}"></script>
+
+
+<!-- Main App -->
+<script src="{{asset('public/js/emrApp.js')}}"></script>
+
+<!-- Controllers -->
+<script src="{{asset('public/js/controllers/expenses.ctrl.js')}}"></script>
+<script src="{{asset('public/js/controllers/revenues.ctrl.js')}}"></script>
+
+<script src="{{asset('public/js/controllers/generalinfo.ctrl.js')}}"></script>
+<script src="{{asset('public/js/controllers/classifications.ctrl.js')}}"></script>
+<script src="{{asset('public/js/controllers/bedcapacities.ctrl.js')}}"></script>
+<script src="{{asset('public/js/controllers/qualitymanagement.ctrl.js')}}"></script>
+
+<script src="{{asset('public/js/controllers/summaryofpatients.ctrl.js')}}"></script>
+<script src="{{asset('public/js/controllers/dischargesnumberdeliveries.ctrl.js')}}"></script>
+<script src="{{asset('public/js/controllers/dischargesopv.ctrl.js')}}"></script>
+<script src="{{asset('public/js/controllers/dischargesev.ctrl.js')}}"></script>
+
+<script src="{{asset('public/js/controllers/surgicaloperationsmajor.ctrl.js')}}"></script>
+<script src="{{asset('public/js/controllers/surgicaloperationsminor.ctrl.js')}}"></script>
+
+<script src="{{asset('public/js/controllers/operationshai.ctrl.js')}}"></script>
+
+<!-- Libraries -->
+<script src="{{asset('public/js/controllers/surgerieslib.ctrl.js')}}"></script>
+
+<!-- Services -->
+<script src="{{asset('public/js/services/expenses.srvcs.js')}}"></script>
+<script src="{{asset('public/js/services/revenues.srvcs.js')}}"></script>
+<script src="{{asset('public/js/services/classifications.srvcs.js')}}"></script>
+<script src="{{asset('public/js/services/bedcapacities.srvcs.js')}}"></script>
+<script src="{{asset('public/js/services/qualitymanagement.srvcs.js')}}"></script>
+
+<script src="{{asset('public/js/services/summaryofpatients.srvcs.js')}}"></script>
+<script src="{{asset('public/js/services/dischargesnumberdeliveries.srvcs.js')}}"></script>
+<script src="{{asset('public/js/services/dischargesopv.srvcs.js')}}"></script>
+<script src="{{asset('public/js/services/dischargesev.srvcs.js')}}"></script>
+
+<script src="{{asset('public/js/services/surgicaloperationsmajor.srvcs.js')}}"></script>
+<script src="{{asset('public/js/services/surgicaloperationsminor.srvcs.js')}}"></script>
+
+<script src="{{asset('public/js/services/operationshai.srvcs.js')}}"></script>
+
+<!-- Libraries -->
+<script src="{{asset('public/js/services/surgerieslib.srvcs.js')}}"></script>
+<script src="{{asset('public/js/services/message.srvcs.js')}}"></script>
+
+ 
+
 @yield('additionalScripts')
